b6e0a509e26be63dd7043b81a270b4f003ca14d8
小风扇 <1517642399@qq.com>
2024-08-16 03:03:32 +0800

    feat: 增加密码创建器

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,41 @@ export default class Toolbox extends Plugin {
 	async onload() {
 		await this.loadSettings();
 		this.addSettingTab(new ToolboxSettingTab(this.app, this));
+		this.settings.passwordCreator &&
+			this.addCommand({
+				id: "密码创建器",
+				name: "密码创建器",
+				callback: () => this.passwordCreator(),
+			});
+	}
+
+	passwordCreator() {
+		const pass = this.pick(
+			this.settings.passwordCreatorMixedContent.split(""),
+			this.settings.passwordCreatorLength
+		).join("");
+		window.navigator.clipboard.writeText(pass);
+		this.notice("密码已复制至剪切板！");
+	}
+
+	pick<T>(arr: T[], n: number = 1, repeat = false): T[] {
+		if (n >= arr.length) {
+			return arr;
+		}
+		let result: T[] = [];
+		let picked: Set<number> = new Set();
+		for (let i = 0; i < n; i++) {
+			let index = Math.floor(Math.random() * arr.length);
+			if (repeat) {
+				while (picked.has(index)) {
+					index = Math.floor(Math.random() * arr.length);
+				}
+				picked.add(index);
+			}
+
+			result.push(arr[index]);
+		}
+		return result;
 	}
 
 	notice(text: string) {
--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,18 @@
 import { App, PluginSettingTab, Setting } from "obsidian";
 import Toolbox from "./main";
 
-export interface ToolboxSettings {}
+export interface ToolboxSettings {
+	passwordCreator: boolean;
+	passwordCreatorMixedContent: string;
+	passwordCreatorLength: number;
+}
 
-export const DEFAULT_SETTINGS: ToolboxSettings = {};
+export const DEFAULT_SETTINGS: ToolboxSettings = {
+	passwordCreator: true,
+	passwordCreatorMixedContent:
+		"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@$%^&*()_+",
+	passwordCreatorLength: 16,
+};
 
 export class ToolboxSettingTab extends PluginSettingTab {
 	plugin: Toolbox;
@@ -17,5 +26,42 @@ export class ToolboxSettingTab extends PluginSettingTab {
 		let { containerEl } = this;
 		containerEl.empty();
 		containerEl.createEl("h2", { text: this.plugin.manifest.name });
+
+		new Setting(containerEl).setName("🔑 密码创建器").addToggle((cd) =>
+			cd
+				.setValue(this.plugin.settings.passwordCreator)
+				.onChange(async (value) => {
+					this.plugin.settings.passwordCreator = value;
+					await this.plugin.saveSettings();
+					this.display();
+				})
+		);
+
+		if (this.plugin.settings.passwordCreator) {
+			new Setting(containerEl)
+				.setName("从哪些字符中随机生成密码？")
+				.addText((cd) =>
+					cd
+						.setValue(
+							"" +
+								this.plugin.settings.passwordCreatorMixedContent
+						)
+						.onChange(async (value) => {
+							this.plugin.settings.passwordCreatorMixedContent =
+								value;
+							await this.plugin.saveSettings();
+						})
+				);
+
+			new Setting(containerEl).setName("生成密码的长度？").addText((cd) =>
+				cd
+					.setValue("" + this.plugin.settings.passwordCreatorLength)
+					.onChange(async (value) => {
+						this.plugin.settings.passwordCreatorLength =
+							Number(value);
+						await this.plugin.saveSettings();
+					})
+			);
+		}
 	}
 }
